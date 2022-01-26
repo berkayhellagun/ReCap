2c40497dfa6560248e1c41e625cf7b08ae28aec4
Berkay Hellagun <berkayhellagun@hotmail.com>
2022-01-26 16:11:29 +0300

    fix

--- Entities/Concrete/Customer.cs
+++ Entities/Concrete/Customer.cs
@@ -11,7 +11,7 @@ namespace Entities.Concrete
     public class Customer:IEntity
     {
         public int Id { get; set; }
-        public int UserI { get; set; }
+        public int UserId { get; set; }
         public string CompanyName { get; set; }
     }
 }
