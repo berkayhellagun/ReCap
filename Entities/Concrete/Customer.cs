6bcdd6d8decdf0d86163573e169fdbbd03a01180
Berkay Hellagun <berkayhellagun@hotmail.com>
2022-01-26 16:08:04 +0300

    validation rules added

--- Entities/Concrete/Customer.cs
+++ Entities/Concrete/Customer.cs
@@ -8,8 +8,10 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    public class Customer:User,IEntity
+    public class Customer:IEntity
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
         public string CompanyName { get; set; }
     }
 }
